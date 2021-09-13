cdb7ccc78362f2a569dc40c7f2414fa2e17bc59b
amanda rice <ariceburner@gmail.com>
2021-09-13 09:12:27 -0600

    removed range

--- keepr/Models/Keep.cs
+++ keepr/Models/Keep.cs
@@ -1,15 +1,12 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace keepr.Models
 {
-    public class Keep
+  public class Keep
     {
     public int Id { get; set; }
     public string CreatorId { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
     public string Img { get; set; }
-    [Range(1, 9000)]
     public int Views{ get; set; }
     public int Shares { get; set; }
     public int Keeps { get; set; }
