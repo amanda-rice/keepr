068a33445b7f3d42eb8cd86c3cd612906876dad5
amanda rice <ariceburner@gmail.com>
2021-09-15 09:43:03 -0600

    actually, we can't deploy to heroku

--- keepr.client/vite.config.js
+++ keepr.client/vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [vue()],
   build: {
-    outDir: '../keepr/client',
+    outDir: 'docs',
     sourcemap: false
   },
   server: {
